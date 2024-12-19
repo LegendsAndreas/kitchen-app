4fad35ab5f96388d1c898cfa45b2c15a5bc74aa0
Andreas <andreas3x3@protonmail.com>
2024-12-19 18:45:09 +0100

    Add navigation on recipe images and update dependencies
    Replaced the "View Details" button with a clickable recipe image for better UI interaction. Updated .NET SDK version and NuGet configurations to support fallback folders and modern SDK analysis. Added fingerprints and integrity attributes to static web assets for enhanced security and compliance.

--- obj/Debug/net8.0/WebKitchen.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebKitchen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebKitchen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3db1c96a44a53085a4ce3ef87e46c8f80290ef7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05266dc915292540a5b26e1fb3410f8933fda56f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebKitchen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebKitchen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
