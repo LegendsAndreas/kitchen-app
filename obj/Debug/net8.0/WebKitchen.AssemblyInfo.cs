cd9fa4d934804077d5498020530c4108ed47b00e
Andreas <andreas3x3@protonmail.com>
2024-12-30 22:30:02 +0100

    Refactor UI and add database diagram asset.
    Updated the layout and styling of "Create Recipe" and related forms for improved user experience. Added "DatabaseDiagram.png" to the project for reference, adjusted scoped CSS and static web assets accordingly. Minor code reformatting and asset configuration changes included.

--- obj/Debug/net8.0/WebKitchen.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebKitchen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebKitchen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9961d2388cfb36fda4a8d92996d309ff7c699c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7837b4af410664de4ee7d1626f3d77e78b7eb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebKitchen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebKitchen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
