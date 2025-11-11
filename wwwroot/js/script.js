23e5741b3fcdcc8a8f02abb19e9ad551e093dfd8
AndreasØstergårdChri <ac@vestjyskmarketing.dk>
2025-11-11 13:56:54 +0100

    Moved the toaster logic into its own script. Each time the StatusMessageToast.razor is instanstizied, a new toaster is made with a unique ID, to make sure it works better. Before it would not work on some pages, works fine now.

---
+++ wwwroot/js/script.js
@@ -0,0 +1,9 @@
+﻿window.showStatusToast = function(toastId) {
+    const toastElement = document.getElementById(toastId);
+    if (toastElement) {
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastElement);
+        toastBootstrap.show();
+    } else {
+        console.error('Toast element not found: ' + toastId);
+    }
+}
